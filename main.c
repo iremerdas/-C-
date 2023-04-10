e12d7561d4a29bd4a6c80f51393a39b3b765e9f9
İrem Erdaş <86477685+iremerdas@users.noreply.github.com>
2023-04-10 23:48:51 +0300

    Update main.c
    Dizinin elemanlarını selection sort algoritması ile lüçükten büyüğe sıralama

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int selection_sort(int arr[], int length){
+int selection_sort(int arr[], int length){  // Fonksiyon parametre olarak sıralamak istenen diziyi ve dizinin uzunluğunu alır
     int min, temp, i ,j;
 
+    /*
+     *  İlk for döngüsü dizinin son elemanını kıyaslamayacağından dizinin uzunluğundan bir eksik dönecektir.
+     *  i'nin min değişkenine eşitlenmesi i sayacının değerini korumak için yapılmıştır ve
+       ilk döngünün her yenilenmesinde değerinin güncellenmesi için ilk for döngüsünün hemen altına yazılmıştır.
+     *  İçteki döngü değişkeni j kıyaslanacak değeri tutuyor gibi düşünülebilir, haliyle i+1 değerini alacaktır.
+    */
     for(i=0;i<length-1;i++){
         min=i;
-        for(j=i+1;j<length-i;j++){
+        for(j=i+1;j<length;j++){
             if(arr[min]>arr[j]){
                 min=j;
             }
         }
+        // Swap işlemi
         temp=arr[i];
         arr[i]=arr[min];
         arr[min]=temp;
@@ -18,11 +25,14 @@ int selection_sort(int arr[], int length){
 
 int main()
 {
-     int length;
+    int length;
     while(1){
-        printf("Enter the number of elements of your array: "); scanf("%d", &length);
+        printf("\nEnter the number of elements of your array: \n(or press '00' to exit!)\n"); scanf("%d", &length);
         int arr[length];
-        for(int i=0;i<uzunluk;i++){
+        if(length==00)
+            break;
+        // Dizinin elemanlarını alma
+        for(int i=0;i<length;i++){
             scanf("%d",&arr[i]);
         }
         printf("\n");
